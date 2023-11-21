6bd7ef1fdee9db923290b3e3da5c8066b0f385dd
alexa <alexa.cerescu@gmail.com>
2023-11-22 00:21:53 +0200

    gata

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
-const cardNumberInput = document.getElementById('cardNumber');
-const displayCardNumber = document.getElementById('displayCardNumber');
+const cardNumberInput = document.getElementById('cardNumber');  
 const imgCompany = document.querySelector("#company_card");
 const form = document.querySelector('form');
 const errorText = document.getElementById('error_text');
@@ -7,49 +6,96 @@ const cardCvvInput = document.querySelector("#cvv")
 const cardCvvDisplay = document.querySelector(".displayCardCvv")
 const cardNameInput = document.getElementById('cardName');
 const displayCardName = document.getElementById('displayCardName');
-
-
-const maskCardNumber = (cardNumber) => {
-    let cardNumbersDisplay = '';
-
-    cardNumber.split('').forEach((character, index) => {
-        if (!isNaN(character) && character !== ' ') {
-            if (index > 0 && index % 4 === 0) {
-                cardNumbersDisplay += ' ';
-            }
-            cardNumbersDisplay += character;
-        }
-    });
-
-    if (cardNumbersDisplay.charAt(0) === '6') {
-        imgCompany.src = "img/amex.png";
-    } else if (cardNumbersDisplay.charAt(0) === '4') {
-        imgCompany.src = "img/visa.png";
-    } else {
-        imgCompany.src = "img/mastercard.png";
-    }
-
-    return cardNumbersDisplay;
-};
-
-
-cardNumberInput.addEventListener('input', (event) => {
-    const inputValue = event.target.value;
-    displayCardNumber.textContent = maskCardNumber(inputValue);
-
-    showError(inputValue, errorText);
-});
-
-// schimb de nume
-
-cardNameInput.addEventListener('input', () => {
-    displayCardName.textContent = cardNameInput.value;
-});
-
-// schimb de data
+const cardNumberDivs = document.querySelectorAll(".card-number-div");
 const inputDate = document.getElementById('inputDate');
 const inputYear = document.getElementById('inputYear');
 const displayCardExpire = document.getElementById('displayCardExpire');
+
+cardNumberInput.addEventListener("input", () => {
+
+    const digits = cardNumberInput.value.split("");
+
+    if (digits[0] === "6") {
+        imgCompany.src = "img/amex.png";
+      } else if (digits[0] === "4") {
+        imgCompany.src = "img/visa.png";
+      }else{
+        imgCompany.src = "img/mastercard.png";
+      }
+    
+
+  cardNumberDivs.forEach((item) => {
+    const cardNumberHiddens = item.querySelectorAll(".card-number-hidden");
+    cardNumberHiddens.forEach((digit) => {
+      digit.textContent = "#";
+    });
+  });
+
+
+
+    digits.forEach((digit, index) => {
+        if (/[^0-9]/.test(digit)) {
+        textError(inputNumber, error, "Please only use digits");
+        } else {
+        const cardNumberHidden = cardNumberDivs[
+            Math.floor(index / 4)
+        ].querySelectorAll(".card-number-hidden")[index % 4];
+
+        cardNumberHidden.textContent = "#";
+
+        if (index === digits.length - 1) {
+            cardNumberHidden.classList.add("slide-up");
+            setTimeout(() => {
+            cardNumberHidden.textContent = digit;
+            cardNumberHidden.classList.remove("slide-up");
+            }, 300);
+        } else {
+            cardNumberHidden.textContent = digit;
+        }
+        }
+    });
+
+});
+
+cardNumberInput.addEventListener("keypress", (event) => {
+    const keyValue = event.key;
+
+    if (isNaN(keyValue) && !event.ctrlKey) {
+        event.preventDefault();
+        const popup = document.getElementById('popup');
+        popup.style.display = 'block';
+        popup.innerText = "Please enter only numbers.";
+
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 2000);
+    }
+});
+
+
+
+// schimb de nume
+
+cardNameInput.addEventListener("input", (event) => {
+    const inputValue = event.target.value;
+    const sanitizedValue = inputValue.replace(/[0-9]/g, '');
+
+    displayCardName.innerText = sanitizedValue !== "" ? sanitizedValue.toUpperCase() : "AD SOYAD";
+
+    if (inputValue !== sanitizedValue) {
+        event.target.value = sanitizedValue; 
+        const popup = document.getElementById('popup');
+        popup.innerText = "Please do not enter numbers in the name.";
+        popup.style.display = 'block';
+
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 2000);
+    }
+});
+
+
+// schimb de data
 
 const updateCardExpire = () => {
     const selectedMonth = inputDate.value;
@@ -88,74 +134,83 @@ cardCvvInput.addEventListener("focusout", toggleCardView);
 
 
 const maskCardCvv = (cardCvv) => {
-    let nr = ""
-   cardCvv.split('').forEach(character => {
-    if (!isNaN(character) && character !== ' ') {
-        nr += character;
-    }
- });
+    let nr = "";
+    cardCvv.split('').forEach(character => {
+        if (!isNaN(character) && character !== ' ') {
+            nr += character;
+        }
+    });
 
     return nr;
 };
 
-cardCvvInput.addEventListener('input', () => {
-    const inputCvv = cardCvvInput.value;
-    cardCvvDisplay.textContent = maskCardCvv(inputCvv);
+cardCvvInput.addEventListener('input', (event) => {
+    const inputCvv = event.target.value;
+    const sanitizedCvv = maskCardCvv(inputCvv);
 
-    showError(inputCvv, errorText);
+    cardCvvDisplay.textContent = sanitizedCvv;
+
+    if (inputCvv !== sanitizedCvv) {
+        event.target.value = sanitizedCvv;
+        const popup = document.getElementById('popup');
+        popup.innerText = "Please enter only numbers.";
+        popup.style.display = 'block';
+
+        setTimeout(() => {
+            popup.style.display = 'none';
+        }, 2000);
+    }
 });
 
 
-const showError = (inputValue, errorElement) => {
-    const containsNonNumeric = /[^0-9]/.test(inputValue);
 
-    if (containsNonNumeric) {
-        errorElement.textContent = 'Please enter only numeric characters.';
-        errorElement.style.display = 'block';
-        return true; 
-    } else {
-        errorElement.style.display = 'none';
-        return false; 
-    }
+
+const cardForm = document.querySelector('#box form');
+
+const resetCard = () => {
+  if (isFlipped) {
+    toggleCardView();
+    isFlipped = false; 
+  }
+}
+
+const handleSubmit = (ev) => {
+    ev.preventDefault();
+    const cardData = {
+      cardNumber: cardNumberInput.value,
+      cardName: cardNameInput.value,
+      expirationMonth: inputDate.value,
+      expirationYear: inputYear.value,
+      cvv: cardCvvInput.value,
+    };
+  
+    console.log(cardData);
+  
+    displayCardName.innerText = 'AD SOYAD';
+  
+    cardCvvDisplay.textContent = '';
+  
+    displayCardExpire.textContent = "MM/YY"
+  
+    cardNumberDivs.forEach((item) => {
+      const cardNumberHiddens = item.querySelectorAll(".card-number-hidden");
+      cardNumberHiddens.forEach((digit) => {
+        digit.textContent = "#";
+      });
+    });
+  
+    resetCard();
+    cardForm.reset();
 };
+  
+cardForm.addEventListener('submit', handleSubmit);
+  
 
-const buttonSubmit = document.querySelector("#submitButton")
-form.addEventListener('submit', (ev) => {
 
-    const cardNumberError = showError(cardNumberInput.value, errorText);
-    const cardCvvError = showError(cardCvvInput.value, errorText);
 
-    if (cardNumberError || cardCvvError) {
-        ev.preventDefault(); 
-        
-        buttonSubmit.style.backgroundColor = "red"
-    }
-});
 
-// const cardForm = document.querySelector('#box form');
 
-// const handleSubmit = (ev) => {
 
-//     ev.preventDefault();
-//     const cardData = {
-//         cardNumber: cardNumberInput.value,
-//         cardName: cardNameInput.value,
-//         expirationMonth: inputDate.value,
-//         expirationYear: inputYear.value,
-//         cvv: cardCvvInput.value,
-//     };
 
-//     // console.log(cardData);
-//     toggleCardView();
-
-//     const cardDataJson = JSON.stringify(cardData)
-//     console.log(cardDataJson)
-
-//     cardForm.reset();
-//     isFlipped = false
-
-// };
-
-// cardForm.addEventListener('submit', handleSubmit);
 
 
